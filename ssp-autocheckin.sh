6276ae63db3cc1968b637588b7c43fde0236f63a
Secret <i@wangmao.me>
2020-12-29 21:25:34 +0800

    多用户合并消息通知

--- ssp-autocheckin.sh
+++ ssp-autocheckin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="2.0.0"
+VERSION="2.1.0"
 
 PATH="/usr/local/bin:/usr/bin:/bin"
 
@@ -24,7 +24,10 @@ fi
 
 users_array=($(echo ${USERS} | tr ';' ' '))
 
+log_text=""
+
 if [ "${users_array}" ]; then
+    user_count=1
     for user in ${users_array[@]}; do
         domain=$(echo ${user} | awk -F'----' '{print $1}')
         username=$(echo ${user} | awk -F'----' '{print $2}')
@@ -39,9 +42,10 @@ if [ "${users_array}" ]; then
         login_code=$(echo ${login} | jq -r '.ret')
         login_status=$(echo ${login} | jq -r '.msg')
 
-        login_log_text="\n\n - 【签到站点】: ${domain}\n"
-        login_log_text="${login_log_text} - 【签到用户】: ${username}\n"
-        login_log_text="${login_log_text} - 【签到时间】: ${start_time}\n"
+        login_log_text="## 用户 ${user_count}\n\n"
+        login_log_text="${login_log_text}- 【签到站点】: ${domain}\n"
+        login_log_text="${login_log_text}- 【签到用户】: ${username}\n"
+        login_log_text="${login_log_text}- 【签到时间】: ${start_time}\n"
 
         if [ ${login_code} == 1 ]; then
             userinfo=$(curl -k -s -G -b ${COOKIE_PATH} "${domain}/getuserinfo")
@@ -74,117 +78,77 @@ if [ "${users_array}" ]; then
                 last_check_in_time_text=$(date -r ${last_check_in_time} '+%Y-%m-%d %H:%M:%S')
             fi
 
-            user_log_text=" - 【用户余额】: ${money} CNY\n"
-            user_log_text="${user_log_text} - 【用户限速】: ${node_speedlimit} Mbps\n"
-            user_log_text="${user_log_text} - 【总流量】: ${transfer_enable_text}\n"
-            user_log_text="${user_log_text} - 【剩余流量】: ${transfer_used_text}\n"
-            user_log_text="${user_log_text} - 【已使用流量】: ${last_day_t_text}\n"
-            user_log_text="${user_log_text} - 【等级过期时间】: ${class_expire}\n"
-            user_log_text="${user_log_text} - 【账户过期时间】: ${expire_in}\n"
-            user_log_text="${user_log_text} - 【上次签到时间】: ${last_check_in_time_text}"
+            user_log_text="- 【用户余额】: ${money} CNY\n"
+            user_log_text="${user_log_text}- 【用户限速】: ${node_speedlimit} Mbps\n"
+            user_log_text="${user_log_text}- 【总流量】: ${transfer_enable_text}\n"
+            user_log_text="${user_log_text}- 【剩余流量】: ${transfer_used_text}\n"
+            user_log_text="${user_log_text}- 【已使用流量】: ${last_day_t_text}\n"
+            user_log_text="${user_log_text}- 【等级过期时间】: ${class_expire}\n"
+            user_log_text="${user_log_text}- 【账户过期时间】: ${expire_in}\n"
+            user_log_text="${user_log_text}- 【上次签到时间】: ${last_check_in_time_text}\n"
 
             checkin=$(curl -k -s -d "" -b ${COOKIE_PATH} "${domain}/user/checkin")
             chechin_code=$(echo ${checkin} | jq -r ".ret")
             checkin_status=$(echo ${checkin} | jq -r ".msg")
 
             if [ "${checkin_status}" ]; then
-                checkin_log_text=" - 【签到状态】: ${checkin_status}\n"
+                checkin_log_text="- 【签到状态】: ${checkin_status}\n\n"
             else
-                checkin_log_text=" - 【签到状态】: 签到失败, 请检查是否存在签到验证码\n"
+                checkin_log_text="- 【签到状态】: 签到失败, 请检查是否存在签到验证码\n\n"
             fi
 
-            result_log_text="${login_log_text}${checkin_log_text}${user_log_text}"
-
-            # echo -e ${result_log_text}
-
-            # Server 酱通知
-            if [ "${PUSH_KEY}" ]; then
-                echo -e "text=${TITLE}&desp=${result_log_text}" >${PUSH_TMP_PATH}
-                push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://sc.ftqq.com/${PUSH_KEY}.send")
-                push_code=$(echo ${push} | jq -r ".errno")
-                if [ ${push_code} -eq 0 ]; then
-                    echo -e "【Server 酱推送结果】: 成功\n"
-                else
-                    echo -e "【Server 酱推送结果】: 失败\n"
-                fi
-            fi
-
-            # Qmsg 酱通知
-            if [ "${QMSG_KEY}" ]; then
-                result_qmsg_log_text="${TITLE}${result_log_text}"
-                echo -e "msg=${result_qmsg_log_text}" >${PUSH_TMP_PATH}
-                push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://qmsg.zendee.cn/send/${QMSG_KEY}")
-                push_code=$(echo ${push} | jq -r ".success")
-                if [ "${push_code}" == "true" ]; then
-                    echo -e "【Qmsg 酱推送结果】: 成功\n"
-                else
-                    echo -e "【Qmsg 酱推送结果】: 失败\n"
-                fi
-            fi
-
-            # TelegramBot 通知
-            if [ "${TELEGRAMBOT_TOKEN}" ] && [ "${TELEGRAMBOT_CHATID}" ]; then
-                result_tgbot_log_text="${TITLE}${result_log_text}"
-                echo -e "chat_id=${TELEGRAMBOT_CHATID}&parse_mode=Markdown&text=${result_tgbot_log_text}" >${PUSH_TMP_PATH}
-                push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://api.telegram.org/bot${TELEGRAMBOT_TOKEN}/sendMessage")
-                push_code=$(echo ${push} | grep -o '"ok":true')
-                if [ ${push_code} ]; then
-                    echo -e "【TelegramBot 推送结果】: 成功\n"
-                else
-                    echo -e "【TelegramBot 推送结果】: 失败\n"
-                fi
-            fi
-
-            rm -rf ${COOKIE_PATH}
-            rm -rf ${PUSH_TMP_PATH}
+            result_log_text="${login_log_text}${checkin_log_text}${user_log_text}\n\n"
         else
-            login_log_text="${login_log_text}【签到状态】: 登录失败, 请检查配置\n"
-            echo -e ${login_log_text}
 
-            # Server 酱通知
-            if [ "${PUSH_KEY}" ]; then
-                echo -e "text=${TITLE}&desp=${login_log_text}" >${PUSH_TMP_PATH}
-                push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://sc.ftqq.com/${PUSH_KEY}.send")
-                push_code=$(echo ${push} | jq -r ".errno")
-                if [ ${push_code} -eq 0 ]; then
-                    echo -e "【Server 酱推送结果】: 成功\n"
-                else
-                    echo -e "【Server 酱推送结果】: 失败\n"
-                fi
-            fi
-
-            # Qmsg 酱通知
-            if [ "${QMSG_KEY}" ]; then
-                result_qmsg_log_text="${TITLE}${login_log_text}"
-                echo -e "msg=${result_qmsg_log_text}" >${PUSH_TMP_PATH}
-                push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://qmsg.zendee.cn/send/${QMSG_KEY}")
-                push_code=$(echo ${push} | jq -r ".success")
-                if [ "${push_code}" == "true" ]; then
-                    echo -e "【Qmsg 酱推送结果】: 成功\n"
-                else
-                    echo -e "【Qmsg 酱推送结果】: 失败\n"
-                fi
-            fi
-
-            # TelegramBot 通知
-            if [ "${TELEGRAMBOT_TOKEN}" ] && [ "${TELEGRAMBOT_CHATID}" ]; then
-                result_tgbot_log_text="${TITLE}${login_log_text}"
-                echo -e "chat_id=${TELEGRAMBOT_CHATID}&parse_mode=Markdown&text=${result_tgbot_log_text}" >${PUSH_TMP_PATH}
-                # push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://api.telegram.org/bot${TELEGRAMBOT_TOKEN}/sendMessage")
-                echo -e ${push}
-                push_code=$(echo ${push} | grep -o '"ok":true')
-                if [ ${push_code} ]; then
-                    echo -e "【TelegramBot 推送结果】: 成功\n"
-                else
-                    echo -e "【TelegramBot 推送结果】: 失败\n"
-                fi
-            fi
-
-            rm -rf ${COOKIE_PATH}
-            rm -rf ${PUSH_TMP_PATH}
+            result_log_text="${login_log_text}-【签到状态】: 登录失败, 请检查配置\n\n"
         fi
-        echo -e "---------------------------------------\n"
+
+        log_text="${log_text}${result_log_text}---------------------------------------\n\n"
+        echo -e ${log_text}
+
+        user_count=$(expr ${user_count} + 1)
     done
+
+    # Server 酱通知
+    if [ "${PUSH_KEY}" ]; then
+        echo -e "text=${TITLE}&desp=${log_text}" >${PUSH_TMP_PATH}
+        push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://sc.ftqq.com/${PUSH_KEY}.send")
+        push_code=$(echo ${push} | jq -r ".errno")
+        if [ ${push_code} -eq 0 ]; then
+            echo -e "【Server 酱推送结果】: 成功\n"
+        else
+            echo -e "【Server 酱推送结果】: 失败\n"
+        fi
+    fi
+
+    # Qmsg 酱通知
+    if [ "${QMSG_KEY}" ]; then
+        result_qmsg_log_text="${TITLE}${log_text}"
+        echo -e "msg=${result_qmsg_log_text}" >${PUSH_TMP_PATH}
+        push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://qmsg.zendee.cn/send/${QMSG_KEY}")
+        push_code=$(echo ${push} | jq -r ".success")
+        if [ "${push_code}" == "true" ]; then
+            echo -e "【Qmsg 酱推送结果】: 成功\n"
+        else
+            echo -e "【Qmsg 酱推送结果】: 失败\n"
+        fi
+    fi
+
+    # TelegramBot 通知
+    if [ "${TELEGRAMBOT_TOKEN}" ] && [ "${TELEGRAMBOT_CHATID}" ]; then
+        result_tgbot_log_text="${TITLE}${log_text}"
+        echo -e "chat_id=${TELEGRAMBOT_CHATID}&parse_mode=Markdown&text=${result_tgbot_log_text}" >${PUSH_TMP_PATH}
+        push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "https://api.telegram.org/bot${TELEGRAMBOT_TOKEN}/sendMessage")
+        push_code=$(echo ${push} | grep -o '"ok":true')
+        if [ ${push_code} ]; then
+            echo -e "【TelegramBot 推送结果】: 成功\n"
+        else
+            echo -e "【TelegramBot 推送结果】: 失败\n"
+        fi
+    fi
+
+    rm -rf ${COOKIE_PATH}
+    rm -rf ${PUSH_TMP_PATH}
 else
     echo "用户组环境变量未配置" && exit 1
 fi
