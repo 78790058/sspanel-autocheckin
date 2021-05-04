394435be772acd56def592f0821f817fce01c0db
rootwhois <bank.of.children@gmail.com>
2021-05-04 11:51:11 +0800

    Update

--- ssp-autocheckin.sh
+++ ssp-autocheckin.sh
@@ -190,13 +190,13 @@ send_message() {
 
     # PushPlus 通知
     if [ "${PUSHPLUS_TOKEN}" ]; then
-        echo -e "title=${TITLE}&desp=${log_text}" >${PUSH_TMP_PATH}
-        push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "http://www.pushplus.plus/send?token=${PUSHPLUS_TOKEN}")
+        echo -e "token=${PUSHPLUS_TOKEN}&title=${TITLE}&content=${log_text}" >${PUSH_TMP_PATH}
+        push=$(curl -k -s --data-binary @${PUSH_TMP_PATH} "http://www.pushplus.plus/send")
         push_code=$(echo ${push} | jq -r ".code" 2>&1)
         if [ ${push_code} -eq 200 ]; then
-            echo -e "【PUSHPLUS 推送结果】: 成功\n"
+            echo -e "【PushPlus 推送结果】: 成功\n"
         else
-            echo -e "【PUSHPLUS 推送结果】: 失败\n"
+            echo -e "【PushPlus 推送结果】: 失败\n"
         fi
     fi
 }
